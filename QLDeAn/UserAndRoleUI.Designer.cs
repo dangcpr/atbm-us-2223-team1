e6ef368cd2b8c6618a185cfe9ac851bf7ccdd32d
Nguyễn Hải Đăng <nguyenthiminhkhai63@gmail.com>
2023-04-02 00:08:30 +0700

    Update UI

--- QLDeAn/UserAndRoleUI.Designer.cs
+++ QLDeAn/UserAndRoleUI.Designer.cs
@@ -29,33 +29,19 @@ namespace QLDeAn
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(156, 75);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(46, 17);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
             // 
             // UserAndRoleUI
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.label1);
             this.Name = "UserAndRoleUI";
             this.Size = new System.Drawing.Size(1261, 620);
             this.Load += new System.EventHandler(this.UserAndRole_Load);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
     }
 }
